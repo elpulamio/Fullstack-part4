7c4d0b7e927c6c98da6faaecc9a5ac644b34112b
elpulamio <lifedowner@gmail.com>
2021-02-24 19:28:13 +0200

    blog list 4.1

---
+++ bloglist_backend/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+app.use(express.json())
+const Blog = require('./models/blog')
+
+const cors = require('cors')
+app.use(cors())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
