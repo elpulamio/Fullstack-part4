d8f0a55281eeacc7806d570348fb8465bac139fb
elpulamio <lifedowner@gmail.com>
2021-02-25 00:52:54 +0200

    eslint + 4.2

--- bloglist_backend/controllers/blogs.js
+++ bloglist_backend/controllers/blogs.js
@@ -2,20 +2,20 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
     })
 })
-  
-blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
     })
     .catch(error => next(error))
 })
