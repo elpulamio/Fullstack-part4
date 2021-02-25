eba71d184e892fab100afdb2e459b9c64f162dec
elpulamio <lifedowner@gmail.com>
2021-02-25 02:50:31 +0200

    4.4

--- bloglist_backend/utils/list_helper.js
+++ bloglist_backend/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
   return 1
 }
-  
-module.exports = {
-  dummy
+
+const totalLikes = (listWithOneBlog) => {
+  return parseInt(String(listWithOneBlog.map(blog => blog.likes)))
 }
+
+module.exports = {
+  dummy,
+  totalLikes
+}
+
