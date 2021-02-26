d6444438bbf540aa54e00524d22dece2e815097b
elpulamio <lifedowner@gmail.com>
2021-02-26 23:05:25 +0200

    4.10

--- bloglist_backend/tests/blog_id.test.js
+++ bloglist_backend/tests/blog_id.test.js
@@ -1,25 +1,24 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-const initialBlog = [
+const initialBlog =
   {
     title: 'Docker and Kubernetes',
     author: 'Bret Fisher',
     url: 'https://www.bretfisher.com/',
     likes: 5
   }
-]
 
 test('identifier is named id', async () => {
   await Blog.deleteMany({})
-  const blogObject = new Blog(initialBlog[0])
-  await blogObject.save()
-  await api
-  expect(blogObject.id).toBeDefined()
+  const blog = new Blog(initialBlog)
+  await blog.save()
+  const response = await api.get('/api/blogs')
+  const blogBody = response.body.map(b => b)
+  expect(blogBody[0].id).toBeDefined()
 })
 
 afterAll(() => {
