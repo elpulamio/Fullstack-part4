d8072ad71d19d915500560c8f4e6bcbd3c49dc56
elpulamio <lifedowner@gmail.com>
2021-02-27 16:28:22 +0200

    creating blogs to user

--- bloglist_backend/controllers/blogs.js
+++ bloglist_backend/controllers/blogs.js
@@ -1,20 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+  const user = await User.findById(body.userId)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  
+  response.json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
