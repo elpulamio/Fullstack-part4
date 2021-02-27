ddbb005a58323826128bdf4c80320be35c8a2bc9
elpulamio <lifedowner@gmail.com>
2021-02-27 17:04:28 +0200

    populate

--- bloglist_backend/controllers/blogs.js
+++ bloglist_backend/controllers/blogs.js
@@ -3,8 +3,10 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs.map(blog => blog.toJSON()))
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
